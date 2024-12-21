6272ca23aadeb77f987b512b7494af7fdbcfc17e
Mark Vz <markimo-the-dev@tztz.io>
2024-12-21 14:03:38 +0100

    Update

--- setup_dev_env_via_mise.sh
+++ setup_dev_env_via_mise.sh
@@ -6,6 +6,13 @@ mise use --global java@latest
 mise use --global kotlin@latest
 mise use --global groovy@latest
 mise use --global python@latest
+mise use --global terraform@latest
+mise use --global kubectl@latest
+mise use --global kubectx@latest
+mise use --global minikube@latest
+mise use --global k9s@latest
+mise use --global helm@latest
+mise use --global rancher@latest
 
 #mise use --global ruby@latest
 #mise use --global rust@latest
--- setup_gnupg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Import GnuPG pubkeys / public keyrings
-
-# Download and import `tarent-everyone.gpg` public keyring
-# See also <https://confluence.tarent.de/pages/viewpage.action?pageId=592937048&focusedCommentId=1470595110#comment-1470595110>
-curl https://tarentpgp.tarent.de/tarent-everyone.gpg | gpg -q --import - &> /dev/null
