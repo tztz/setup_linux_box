665dba01b98626fe53b2dcf0423dacc4e2d75111
Mark Vz <markimo-the-dev@tztz.io>
2025-01-06 10:11:55 +0100

    Remove Python

--- setup_dev_env_via_mise.sh
+++ setup_dev_env_via_mise.sh
@@ -5,7 +5,6 @@ mise use --global go@latest
 mise use --global java@latest
 mise use --global kotlin@latest
 mise use --global groovy@latest
-mise use --global python@latest
 mise use --global terraform@latest
 mise use --global kubectl@latest
 mise use --global kubectx@latest
@@ -13,18 +12,3 @@ mise use --global minikube@latest
 mise use --global k9s@latest
 mise use --global helm@latest
 mise use --global rancher@latest
-
-#mise use --global ruby@latest
-#mise use --global rust@latest
-#mise use --global swift@latest
-#mise use --global dart@latest
-#mise use --global php@latest
-#mise use --global perl@latest
-#mise use --global lua@latest
-#mise use --global r@latest
-#mise use --global julia@latest
-#mise use --global scala@latest
-#mise use --global clojure@latest
-#mise use --global haskell@latest
-#mise use --global elixir@latest
-#mise use --global erlang@latest
