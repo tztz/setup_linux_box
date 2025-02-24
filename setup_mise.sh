c432e9e1110297eae0678df6159dfed74c8f5cc8
Mark Vz <markimo-the-dev@tztz.io>
2025-02-24 15:52:00 +0100

    Add check

--- setup_mise.sh
+++ setup_mise.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+mise --version &> /dev/null
+mise_exists=$?
+if [ $mise_exists -eq 0 ]; then
+    echo "mise already exists. Skipping."
+    exit 0
+fi
+
 curl https://mise.run | sh
 echo 'eval "$(~/.local/bin/mise activate bash)"' >> ~/.bashrc
 mise doctor
