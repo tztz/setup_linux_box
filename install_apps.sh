631d10c6e2e4f5db165f082fb758ba2a5a083839
Mark Vz <markimo-the-dev@tztz.io>
2025-05-03 12:46:52 +0200

    Update

--- install_apps.sh
+++ install_apps.sh
@@ -5,13 +5,19 @@ if [[ "$BASE_FOLDER" == '' ]]; then
 fi
 
 OPERATING_SYSTEM=$(uname -o)
+LINUX_DISTRIBUTION=$(cat /etc/os-release | sed -nE 's/^NAME="(.*)"$/\1/p')
 
-if [[ "$OPERATING_SYSTEM" == 'GNU/Linux' ]]; then
+if [[ "${OPERATING_SYSTEM}" == 'GNU/Linux' ]]; then
     # Install Linux apps ...
 
     # Install classic packages
-    yay -S --needed --noconfirm - < $BASE_FOLDER/pkglist_yay.txt
-    #xargs sudo apt-get -y install < pkglist_aptget.txt
+    if [[ "${LINUX_DISTRIBUTION}" == 'EndeavourOS' || "${LINUX_DISTRIBUTION}" == 'Arch Linux' ]]; then
+        echo "Installing via yay ..."
+        yay -S --needed --noconfirm - < $BASE_FOLDER/pkglist_yay.txt
+    else
+        echo "Installing via apt-get ..."
+        xargs sudo apt-get -y install < pkglist_aptget.txt
+    fi
 
     # Install flatpak packages
     flatpak -y install $(cat $BASE_FOLDER/pkglist_flatpak.txt)
