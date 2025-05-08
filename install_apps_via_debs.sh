a2beeff2f571df2f0f1cde5e5a54bc2b526dbe8b
Mark Vz <markimo-the-dev@tztz.io>
2025-05-08 12:37:01 +0200

    Create install_apps_via_debs.sh

---
+++ install_apps_via_debs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p /tmp/debs001
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/debs001/google-chrome-stable_current_amd64.deb
+wget https://www.synaptics.com/sites/default/files/Ubuntu/pool/stable/main/all/synaptics-repository-keyring.deb -O /tmp/debs001/synaptics-repository-keyring.deb
+
+sudo apt install /tmp/debs001/google-chrome-stable_current_amd64.deb
+sudo apt install /tmp/debs001/synaptics-repository-keyring.deb
+
+sudo apt update
+
+sudo apt install google-chrome-stable
+sudo apt install displaylink-driver
