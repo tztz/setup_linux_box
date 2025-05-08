868619a3e9c3203797df47f3c8089b7814cb568a
Mark Vz <markimo-the-dev@tztz.io>
2025-05-08 15:57:25 +0200

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Check whether docker is already installed
+docker --version > /dev/null
+docker_version=$?
+if [ $docker_version != '' ]; then
+    echo "docker is already installed. Skipping."
+    exit 0
+fi
+
 mkdir -p /tmp/docker_install
 curl -fsSL https://get.docker.com -o /tmp/docker_install/get-docker.sh && \
 sudo sh /tmp/docker_install/get-docker.sh
