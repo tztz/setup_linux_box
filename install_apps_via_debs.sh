4a525c1ce06de6834eddcd045750f7111e0e494d
Mark Vz <markimo-the-dev@tztz.io>
2025-05-08 16:08:39 +0200

    Update install_apps_via_debs.sh

--- install_apps_via_debs.sh
+++ install_apps_via_debs.sh
@@ -4,7 +4,7 @@ mkdir -p /tmp/debs001
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/debs001/google-chrome-stable_current_amd64.deb
 wget https://www.synaptics.com/sites/default/files/Ubuntu/pool/stable/main/all/synaptics-repository-keyring.deb -O /tmp/debs001/synaptics-repository-keyring.deb
-wget https://vscode.download.prss.microsoft.com/dbazure/download/stable/17baf841131aa23349f217ca7c570c76ee87b957/code_1.99.3-1744761595_amd64.deb -O /tmp/debs001/vscode.deb
+wget https://go.microsoft.com/fwlink/?LinkID=760868 -O /tmp/debs001/vscode.deb
 
 sudo apt install /tmp/debs001/google-chrome-stable_current_amd64.deb
 sudo apt install /tmp/debs001/synaptics-repository-keyring.deb
