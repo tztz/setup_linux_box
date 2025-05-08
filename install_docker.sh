c199a53d3fcccdade642df80e2c7f9279e99c459
Mark Vz <markimo-the-dev@tztz.io>
2025-05-08 13:12:26 +0200

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,3 +1,4 @@
- curl -fsSL https://get.docker.com -o /tmp/docker_install/get-docker.sh
- sudo sh /tmp/docker_install/get-docker.sh --dry-run
- 
+#!/bin/bash
+
+curl -fsSL https://get.docker.com -o /tmp/docker_install/get-docker.sh && \
+sudo sh /tmp/docker_install/get-docker.sh --dry-run
