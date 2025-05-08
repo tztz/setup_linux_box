c892ecb480d6314ecf4fcda78ff41e59adc74ed1
Mark Vz <markimo-the-dev@tztz.io>
2025-05-08 14:29:58 +0000

    Fix 2

--- download_files.sh
+++ download_files.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Downloads some interesting files, installers, etc.
-wget https://www.jetbrains.com/toolbox-app/download/download-thanks.html?platform=linux -O ~/Downloads
+wget https://www.jetbrains.com/toolbox-app/download/download-thanks.html?platform=linux -O ~/Downloads/jetbrains-toolbox-current.tar.gz
