07b9787461b4c42d0e9a934b2fe570e179f07757
Mark Vz <markimo-the-dev@tztz.io>
2025-05-09 18:59:16 +0200

    Update

--- setup_linux_box.sh
+++ setup_linux_box.sh
@@ -84,7 +84,7 @@ $BASE_FOLDER/setup_java_truststore.sh
 
 print_headline "Install mise"
 
-###$BASE_FOLDER/setup_mise.sh
+$BASE_FOLDER/setup_mise.sh
 
 ####################################################################################
 # Install dev tools via mise
@@ -94,7 +94,7 @@ print_headline "Install mise"
 
 print_headline "Install dev tools via mise"
 
-###$BASE_FOLDER/setup_dev_env_via_mise.sh
+$BASE_FOLDER/setup_dev_env_via_mise.sh
 
 ####################################################################################
 # Global npm packages
