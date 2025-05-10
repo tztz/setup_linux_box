0000d4679bb635c733b9c85da23a85d3e7b80052
Mark Vz <markimo-the-dev@tztz.io>
2025-05-10 16:13:39 +0200

    Update

--- install_apps_via_debs.sh
+++ install_apps_via_debs.sh
@@ -5,10 +5,12 @@ mkdir -p /tmp/debs001
 # ---
 wget 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb' -O /tmp/debs001/google-chrome-stable_current_amd64.deb
 wget 'https://go.microsoft.com/fwlink/?LinkID=760868' -O /tmp/debs001/vscode.deb
+wget 'https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_136.0.3240.64-1_amd64.deb' -O /tmp/debs001/microsoft-edge-stable_amd64.deb
 
 # ---
 sudo apt install /tmp/debs001/google-chrome-stable_current_amd64.deb
 sudo apt install /tmp/debs001/vscode.deb
+sudo apt install /tmp/debs001/microsoft-edge-stable_amd64.deb
 
 # ---
 sudo apt update
