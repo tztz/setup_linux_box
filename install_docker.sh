d02eab5befbb2c106570f09aafe446c9fc405275
Mark Vz <markimo-the-dev@tztz.io>
2025-05-10 12:36:16 +0200

    Update

--- install_docker.sh
+++ install_docker.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-# Check whether docker is already installed
-if command -v docker 2>&1 >/dev/null; then
-    echo "docker is already installed. Skipping."
-    exit 0
-fi
+# Add Docker's official GPG key:
+sudo apt update
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-mkdir -p /tmp/docker_install
-curl -fsSL https://get.docker.com -o /tmp/docker_install/get-docker.sh && \
-sudo sh /tmp/docker_install/get-docker.sh
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+
+wget 'https://desktop.docker.com/linux/main/amd64/docker-desktop-amd64.deb' -O /tmp/debs001/docker-desktop-amd64.deb
+
+sudo apt install /tmp/debs001/docker-desktop-amd64.deb
