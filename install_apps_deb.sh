2184db897921ab1420fc569dddda6e12185da270
Mark Vz <markimo-the-dev@tztz.io>
2025-11-21 11:09:52 +0100

    Add rows

--- install_apps_deb.sh
+++ install_apps_deb.sh
@@ -23,12 +23,16 @@ install_deb() {
 download_deb 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb' 'google-chrome-stable_current_amd64.deb'
 download_deb 'https://go.microsoft.com/fwlink/?LinkID=760868' 'vscode.deb'
 download_deb 'https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_136.0.3240.64-1_amd64.deb' 'microsoft-edge-stable_amd64.deb'
+#download_deb 'https://kafkio.com/download/kafkio/2.0.13/KafkIO-linux-2.0.13-x64.deb' 'kafkio.deb'
+#download_deb 'https://www.keepersecurity.com/desktop_electron/Linux/repo/deb/keeperpasswordmanager_17.4.1_amd64.deb' 'keeperpasswordmanager.deb'
 
 # ---
 
 install_deb 'google-chrome-stable_current_amd64.deb'
 install_deb 'vscode.deb'
 install_deb 'microsoft-edge-stable_amd64.deb'
+#install_deb 'kafkio.deb'
+#install_deb 'keeperpasswordmanager.deb'
 
 # ---
 
